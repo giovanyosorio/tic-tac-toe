8c1a7aad420a17cfa0236bc93d75f3b48c28f8ba
giovany osorio <30839218+giovanyosorio@users.noreply.github.com>
2023-10-19 02:01:46 +0000

    detectar el ganador

--- src/App.jsx
+++ src/App.jsx
@@ -19,14 +19,45 @@ const Square=({children,isSelected,updateBoard,index}) => {
     </div>
   )
 }
+
+const WINNER_COMBINATIONS = [
+  // horizontal
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  // vertical
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  // diagonal
+  [0,4,8],
+  [2,4,6]
+]
 function App() {
   const [board,setBoard]= useState(Array(9).fill(null))
   
   const [turn,setTurn]= useState(TURNS.X)
+  const [winner,setWinner]= useState(null) // null no hay ganador, false empate
+
+  const checkWinner = (boardtoCheck) => {
+    // recorrer las combinaciones ganadoras
+    for(const combo of WINNER_COMBINATIONS){
+      const[a,b,c]=combo
+      // si las casillas de la combinacion actual tienen el mismo valor
+      if(boardtoCheck[a]  &&
+         boardtoCheck[a] === boardtoCheck[b] && 
+         boardtoCheck[a] === boardtoCheck[c]){
+        // retornar el valor del ganador
+        return boardtoCheck[a]
+      }
+    }
+    // si no hay ganador
+    return null
+  }
 
     const updateBoard = (index) => {
       // si la casilla ya esta ocupada no hacer nada
-      if(board[index]){
+      if(board[index] || winner){
         return
       }
       // actualizar el tablero
@@ -36,6 +67,14 @@ function App() {
       // cambiar el turno
       const newTurn= turn === TURNS.X ? TURNS.O : TURNS.X
       setTurn(newTurn)
+      // verificar si hay ganador
+      const newWinner = checkWinner(newBoard)
+      if(newWinner){
+        //alert(`El ganador es ${newWinner}`)
+        setWinner(newWinner)
+        console.log('El ganador es', newWinner);
+      }
+
   }
 
   return (
